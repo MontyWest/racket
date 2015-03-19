77adbef4d3de778d58af203729b773766397f4cf
MontyWest <monty.p.west@gmail.com>
2015-03-19 19:54:16 +0000

    transform funcs plus get row col box

--- file-test.rkt
+++ file-test.rkt
@@ -5,3 +5,28 @@
 (check-equal? all (set 1 2 3 4 5 6 7 8 9) "Test all")
 (check-equal? (tile-solved? all) #f "Testing that more that one option returns not solved")
 (check-equal? (tile-solved? (set 3)) #t "Testing singleton returns solved")
+
+
+(check-equal? (transform (list (list 1 2 0) (list 0 5 0) (list 7 0 9))) 
+              (list (list (set 1) (set 2) (set 1 2 3 4 5 6 7 8 9))
+                    (list (set 1 2 3 4 5 6 7 8 9) (set 5) (set 1 2 3 4 5 6 7 8 9))
+                    (list (set 7) (set 1 2 3 4 5 6 7 8 9) (set 9)))
+              "Testing transforming small grid into nest lists of sets")
+
+(check-equal? (transformflat (list (list 1 2 0) (list 0 5 0) (list 7 0 9))) 
+              (list (set 1) (set 2) (set 1 2 3 4 5 6 7 8 9)
+                    (set 1 2 3 4 5 6 7 8 9) (set 5) (set 1 2 3 4 5 6 7 8 9)
+                    (set 7) (set 1 2 3 4 5 6 7 8 9) (set 9))
+              "Testing transforming small grid into flat list of sets")
+
+(check-equal? (get-row 80) 8 "Testing tile 80 is in row 8")
+(check-equal? (get-row 54) 6 "Testing tile 54 is in row 6")
+(check-equal? (get-row 4) 0 "Testing tile 4 is in row 0")
+
+(check-equal? (get-col 80) 8 "Testing tile 80 is in col 8")
+(check-equal? (get-col 54) 0 "Testing tile 55 is in col 0")
+(check-equal? (get-col 4) 4 "Testing tile 4 is in col 4")
+
+(check-equal? (get-box 80) 8 "Testing tile 80 is in box 8")
+(check-equal? (get-box 54) 6 "Testing tile 55 is in box 6")
+(check-equal? (get-box 4) 1 "Testing tile 4 is in box 1")
--- file.rkt
+++ file.rkt
@@ -4,8 +4,34 @@
 
 (define (tile-solved? t) (= 1 (set-count t)))
 
+(define (transform matrix) 
+  (map 
+   (lambda (lst) 
+     (map
+      (lambda (num) (
+         if (equal? num 0) all (set num))
+        )
+      lst))
+   matrix))
+
+(define (transformflat matrix) 
+  (map 
+      (lambda (num) (
+         if (equal? num 0) all (set num))
+        )
+   (flatten matrix)))
+
+(struct tile (row col box values) #:transparent)
+
+(define (get-row pos) (quotient pos 9))
+(define (get-col pos) (- pos (* 9 (get-row pos))))
+(define (get-box pos) (+ (quotient (get-col pos) 3) (* 3 (quotient (get-row pos) 3))))
+
+(define (make-tile value pos) (if (equal? value 0) (tile 1 2 3 all) (tile 2 4 5 (set value))))
+                               
+
+(define (transform-tile matrix) 
+  (map make-tile (flatten matrix)))
 
 
-
-
-(provide all tile-solved?)
+(provide all tile-solved? transform transformflat tile get-row get-col get-box)
